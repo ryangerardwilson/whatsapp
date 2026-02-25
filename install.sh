b8a497df711f8ea18eaa43f421c8adca1760a363
Ryan Gerard Wilson <ryan@wilsonfamilyoffice.in>
2026-02-25 09:44:49 +0530

    sync

--- install.sh
+++ install.sh
@@ -155,9 +155,9 @@ else
   "$VENV_DIR/bin/python" -m pip install -r "$APP_DIR/requirements.txt"
   if [[ "$(uname -s)" == "Linux" ]] && command -v pacman >/dev/null 2>&1; then
     print_message info "${MUTED}Arch detected. You may need Playwright deps:${NC}"
-    print_message info "  sudo pacman -S --needed glibc libx11 libxcomposite libxdamage libxfixes libxrandr \\\n+    libxkbcommon libxkbcommon-x11 libxcb libxext libxrender libdrm libegl libglvnd mesa \\\n+    at-spi2-core atk cairo pango alsa-lib cups libxshmfence nss nspr openssl fontconfig \\\n+    freetype2 harfbuzz libjpeg-turbo libpng libwebp"
+    print_message info "  sudo pacman -S --needed glibc libx11 libxcomposite libxdamage libxfixes libxrandr libxkbcommon libxkbcommon-x11 libxcb libxext libxrender libdrm libegl libglvnd mesa at-spi2-core atk cairo pango alsa-lib cups libxshmfence nss nspr openssl fontconfig freetype2 harfbuzz libjpeg-turbo libpng libwebp"
   fi
-  "$VENV_DIR/bin/python" -m playwright install
+  PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS=1 "$VENV_DIR/bin/python" -m playwright install
 
   cat > "${INSTALL_DIR}/${APP}" <<EOF
 #!/usr/bin/env bash
